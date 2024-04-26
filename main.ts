02968fc0c678956c3bf24f89b615ef1ddf54bb8d
furqansalam <166218116+furqansalam@users.noreply.github.com>
2024-04-26 16:42:38 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+let personName: string = "Babar"
+console.log("lowercase:", personName.toLowerCase());
+
+console.log("uppercase:", personName.toLocaleUpperCase());
+
+console.log("titlecase:", personName.replace(/\bw/g,c=> c.toUpperCase()));
